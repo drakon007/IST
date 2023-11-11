2ebddb69bf28f39a0adf0824105c7f3d9cba2dec
Иванов Данил Александрович <20061@iat.iat>
2023-11-11 14:16:48 +0800

    Приведение в корректную форму пагинации на странице вывода тестов Нужно сделать: 1) пагинацию вопросов на странице тестов с проверкой 2) сохранение результатов 3) вывод результатов 4) страницы добавления тестов инторпретаций и вопросов

--- resources/views/test/home_page.blade.php
+++ resources/views/test/home_page.blade.php
@@ -18,14 +18,17 @@
             <div class="flex w-full lg:w-1/2 md:w-1/3 xl:w-1/3 flex-wrap">
                 <div class="w-full rounded ">
                     <div class="px-6 py-4">
-                        <div class="font-bold text-xl mb-2 text-center md:text-left text-4xl md:text-3xl"> {{$test->name}}</div>
+                        <div
+                            class="font-bold text-xl mb-2 text-center md:text-left text-4xl md:text-3xl"> {{$test->name}}</div>
                         {{--<p class="text-gray-700 text-base">Описаниe</p>--}}
                         {{--// todo если надо будет добавить описание к тесту--}}
                     </div>
                     <div class="text-center md:text-left px-6 pt-4 pb-2  text-xl md:text-base">
                         @if ($test->status == "active")
                             <a href="http://127.0.0.1:8000/question/get/{{$test->id}}">
-                                <button class="bg-blue-500 hover:bg-blue-700 text-white rounded-full px-3 py-1  font-semibold text-gray-700 mr-2 mb-2"type="button">
+                                <button
+                                    class="bg-blue-500 hover:bg-blue-700 text-white rounded-full px-3 py-1  font-semibold text-gray-700 mr-2 mb-2"
+                                    type="button">
                                     Пройти
                                 </button>
                             </a>
@@ -39,112 +42,52 @@
         @endforeach
     </div>
 </div>
-@if ($tests->hasPages())
-    <nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="flex items-center justify-between">
-        <div class="flex justify-between flex-1 sm:hidden">
-            @if ($tests->onFirstPage())
-                <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md">
-                    {!! __('pagination.previous') !!}
-                </span>
-            @else
-                <a href="{{ $tests->previousPageUrl() }}" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-                    {!! __('pagination.previous') !!}
-                </a>
-            @endif
 
-            @if ($tests->hasMorePages())
-                <a href="{{ $tests->nextPageUrl() }}" class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-                    {!! __('pagination.next') !!}
-                </a>
-            @else
-                <span class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md">
-                    {!! __('pagination.next') !!}
-                </span>
-            @endif
-        </div>
-
-        <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+<footer class="flex">
+    <nav class=" mx-auto flex gap-0.5">
+        {{--если текущая страница не 1 тогда отрисовать кнопку назад--}}
+        @if($tests->currentPage() > 1)
             <div>
-                <p class="text-sm text-gray-700 leading-5">
-                    {!! __('Сейчас показаны  тесты от') !!}
-                    @if ($tests->firstItem())
-                        <span class="font-medium">{{ $tests->firstItem() }}</span>
-                        {!! __('до') !!}
-                        <span class="font-medium">{{ $tests->lastItem() }}</span>
-                    @else
-                        {{ $tests->count() }}
-                    @endif
-                    {!! __('из') !!}
-                    <span class="font-medium">{{ $tests->total() }}</span>
-                    {!! __('тестов') !!}
-                </p>
+                <a href="{{$tests->previousPageUrl()}}"
+                   class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-blue-500  hover:text-white">
+                    <svg class="w-3.5 h-3.5 me-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                         fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M13 5H1m0 0 4 4M1 5l4-4"/>
+                    </svg>
+                    Предыдущая
+                </a>
             </div>
+        @endif
 
+        {{--если страниц несколько, тогда вывесити ссылки на них--}}
+        @if($tests->lastPage() >=2)
+            @for ($i = 0; $i < $tests->lastPage(); $i++)
+                <div class="justify-center">
+                    <a href="{{$tests->url($i+1)}}"
+                       class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-blue-500  hover:text-white">
+                        {{$i+1}}
+                    </a>
+                </div>
+            @endfor
+        @endif
+
+        {{--если это не последняя страница, отрисовать кнопку далее--}}
+        @if($tests->currentPage() < $tests->lastPage())
             <div>
-                <span class="relative z-0 inline-flex shadow-sm rounded-md">
-                    {{-- Previous Page Link --}}
-                    @if ($tests->onFirstPage())
-                        <span aria-disabled="true" aria-label="{{ __('pagination.previous') }}">
-                            <span class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default rounded-l-md leading-5" aria-hidden="true">
-                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
-                                </svg>
-                            </span>
-                        </span>
-                    @else
-                        <a href="{{ $tests->previousPageUrl() }}" rel="prev" class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-l-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" aria-label="{{ __('pagination.previous') }}">
-                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                                <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
-                            </svg>
-                        </a>
-                    @endif
-
-{{--                    --}}{{-- Pagination Elements --}}
-{{--                    @foreach ($elements as $element)--}}
-{{--                        --}}{{-- "Three Dots" Separator --}}
-{{--                        @if (is_string($element))--}}
-{{--                            <span aria-disabled="true">--}}
-{{--                                <span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 cursor-default leading-5">{{ $element }}</span>--}}
-{{--                            </span>--}}
-{{--                        @endif--}}
-
-{{--                        --}}{{-- Array Of Links --}}
-{{--                        @if (is_array($element))--}}
-{{--                            @foreach ($element as $page => $url)--}}
-{{--                                @if ($page == $tests->currentPage())--}}
-{{--                                    <span aria-current="page">--}}
-{{--                                        <span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5">{{ $page }}</span>--}}
-{{--                                    </span>--}}
-{{--                                @else--}}
-{{--                                    <a href="{{ $url }}" class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150" aria-label="{{ __('Go to page :page', ['page' => $page]) }}">--}}
-{{--                                        {{ $page }}--}}
-{{--                                    </a>--}}
-{{--                                @endif--}}
-{{--                            @endforeach--}}
-{{--                        @endif--}}
-{{--                    @endforeach--}}
-
-{{--                    --}}{{-- Next Page Link --}}
-                    @if ($tests->hasMorePages())
-                        <a href="{{ $tests->nextPageUrl() }}" rel="next" class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-r-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" aria-label="{{ __('pagination.next') }}">
-                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-                            </svg>
-                        </a>
-                    @else
-                        <span aria-disabled="true" aria-label="{{ __('pagination.next') }}">
-                            <span class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default rounded-r-md leading-5" aria-hidden="true">
-                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-                                </svg>
-                            </span>
-                        </span>
-                    @endif
-                </span>
+                <a href="{{$tests->nextPageUrl()}}"
+                   class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-blue-500  hover:text-white">
+                    Следущая
+                    <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                         fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
+                </a>
             </div>
-        </div>
+        @endif
     </nav>
-@endif
+</footer>
 
 
 </body>
